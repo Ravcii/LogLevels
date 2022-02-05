99f9d4af3b2b52635edfd516d0624e1180cd36fd
Ravsii <naplle58@gmail.com>
2022-02-05 14:42:24 +0300

    Fix interface parametrs

--- loglevels.go
+++ loglevels.go
@@ -52,7 +52,7 @@ func Debug(str string) {
 
 // Debugf outputs a formatted debug-level log
 func Debugf(format string, v ...interface{}) {
-	Output(DEBUG, format, v)
+	Output(DEBUG, format, v...)
 }
 
 // Info outputs an info-level log
@@ -62,7 +62,7 @@ func Info(str string) {
 
 // Infof outputs a formatted info-level log
 func Infof(format string, v ...interface{}) {
-	Output(INFO, format, v)
+	Output(INFO, format, v...)
 }
 
 // Warning outputs a warning-level log
@@ -72,7 +72,7 @@ func Warning(str string) {
 
 // Warningf outputs a formatted warning-level log
 func Warningf(format string, v ...interface{}) {
-	Output(WARNING, format, v)
+	Output(WARNING, format, v...)
 }
 
 // Error outputs an error-level log
@@ -82,7 +82,7 @@ func Error(str string) {
 
 // Errorf outputs a formatted error-level log
 func Errorf(format string, v ...interface{}) {
-	Output(ERROR, format, v)
+	Output(ERROR, format, v...)
 }
 
 // Output sends formatted string to a default logger (without level checking!)
